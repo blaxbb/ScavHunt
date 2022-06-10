3f79763bf90c7bae6c3d3ef4cb95d53445532afa
Brian Barrett <blaxbb@gmail.com>
2022-06-10 01:03:15 -0400

    Scaffold and enable 2fa

---
+++ ScavHunt/wwwroot/js/qrcode/qrcode.js
@@ -0,0 +1,12 @@
+﻿// from - https://docs.microsoft.com/en-us/aspnet/core/security/authentication/identity-enable-qrcodes?view=aspnetcore-6.0
+// for scaffolding 2fa qrcode generation
+
+window.addEventListener("load", () => {
+    const uri = document.getElementById("qrCodeData").getAttribute('data-url');
+    new QRCode(document.getElementById("qrCode"),
+        {
+            text: uri,
+            width: 150,
+            height: 150
+        });
+});
