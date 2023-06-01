d67a27cc5cdf999465cdf9b9f7366a9773b558f9
Brian Barrett <blaxbb@gmail.com>
2023-05-31 23:36:28 -0400

    Revert apple auth change

--- ScavHunt/Program.cs
+++ ScavHunt/Program.cs
@@ -59,7 +59,7 @@ if(builder.Configuration["Authentication:Apple:ClientId"] != null) {
             options.ClientId = builder.Configuration["Authentication:Apple:ClientId"];
             options.KeyId = builder.Configuration["Authentication:Apple:KeyId"];
             options.TeamId = builder.Configuration["Authentication:Apple:TeamId"];
-            options.GenerateClientSecret = true;
+
             options.PrivateKey = (keyId, _) =>
             {
                 return Task.FromResult(builder.Configuration[$"Authentication:Apple:{keyId}"].AsMemory());
