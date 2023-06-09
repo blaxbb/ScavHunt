b3662a7b3ceb14a8df85b2c15d77ccbd20848337
Brian Barrett <blaxbb@gmail.com>
2023-06-08 23:21:57 -0400

    Add send bulk email to Alerts admin page

--- ScavHunt/Services/EmailSender.cs
+++ ScavHunt/Services/EmailSender.cs
@@ -40,5 +40,38 @@ namespace ScavHunt.Services
 
             logger.LogInformation(response.IsSuccessStatusCode ? $"Email to {email} queued" : $"Email to {email} failed");
         }
+
+        public async Task SendEmailAsync(List<string> emails, string subject, string message)
+        {
+            if (string.IsNullOrWhiteSpace(options.Key))
+            {
+                throw new Exception("Send grid key is not defined.");
+            }
+
+            var client = new SendGridClient(options.Key);
+
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(options.FromEmail, "SIGGRAPH 2023 - Scavenger Hunt"),
+                Subject = subject,
+                PlainTextContent = message.Replace("<br />", "\n"),
+                HtmlContent = message,
+            };
+
+            msg.Personalizations = new List<Personalization>();
+
+            msg.Personalizations.AddRange(emails.Select(e =>
+            
+                new Personalization()
+                {
+                    Tos = new() { new EmailAddress(e) }
+                }
+            ));
+
+            msg.SetClickTracking(false, false);
+            var response = await client.SendEmailAsync(msg);
+
+            logger.LogInformation(response.IsSuccessStatusCode ? $"Bulk email to {emails.Count} queued" : $"Bulk emails to {emails.Count} failed");
+        }
     }
 }
