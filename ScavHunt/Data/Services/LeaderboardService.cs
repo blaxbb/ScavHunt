f1a6ff75b51c3da25718e8addceb429eb16e55e5
Brian Barrett <blaxbb@gmail.com>
2023-06-30 00:22:53 -0400

    Added email leaders feature

--- ScavHunt/Data/Services/LeaderboardService.cs
+++ ScavHunt/Data/Services/LeaderboardService.cs
@@ -31,5 +31,19 @@ namespace ScavHunt.Data.Services
             var leaderboard = await Leaderboard();
             return leaderboard.FindIndex(p => p.User.Id == player.User.Id) + 1;
         }
+
+        public string PointString(List<Player> LeaderboardData, Player player)
+        {
+            var points = player.PointTransactions.Sum(p => p.Value);
+            var others = LeaderboardData?.Count(other => other.PointTransactions.Sum(p => p.Value) == points);
+            if (others > 1)
+            {
+                return $"{player.PointTransactions.Sum(p => p.Value)} ({player.PointTransactions.Sum(p => p.Duration).ToString("F2")} seconds)";
+            }
+            else
+            {
+                return player.PointTransactions.Sum(p => p.Value).ToString();
+            }
+        }
     }
 }
