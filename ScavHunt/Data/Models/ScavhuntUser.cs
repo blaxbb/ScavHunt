d8248cf1657e1d20f0db555daef9433017d8865f
Brian Barrett <blaxbb@gmail.com>
2023-07-02 23:12:38 -0400

    allow toggling email confirmed from admin edit, include scavhuntuser properties when exporting user data

--- ScavHunt/Data/Models/ScavhuntUser.cs
+++ ScavHunt/Data/Models/ScavhuntUser.cs
@@ -5,9 +5,11 @@ namespace ScavHunt.Data.Models
 {
     public class ScavhuntUser : IdentityUser
     {
+        [PersonalData]
         public string? DisplayName { get; set; }
 
         [StringLength(255)]
+        [PersonalData]
         public string? BadgeId { get; set; }
 
         public const string BADGE_REGEX = "^http:\\/\\/rcsreg\\.com\\/(?:.*?\\/){8}$";
